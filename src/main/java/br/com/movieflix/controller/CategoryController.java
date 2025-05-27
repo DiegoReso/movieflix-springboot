cd9ee65f4bfa1d9466a425ff5c9a09d7bde5db6d
Diego Reis <diegoreso@me.com>
2025-05-27 15:35:31 -0300

    feat: creates Category entity, CategoryService, CategoryController and CategoryRepository

--- src/main/java/br/com/movieflix/controller/CategoryController.java
+++ src/main/java/br/com/movieflix/controller/CategoryController.java
@@ -1,4 +1,28 @@
 package br.com.movieflix.controller;
 
+
+import br.com.movieflix.entity.Category;
+import br.com.movieflix.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movieflix/category")
+@RequiredArgsConstructor
 public class CategoryController {
+
+    private final CategoryService service;
+
+
+    @GetMapping
+    public List<Category> getAllCategories(){
+        List<Category> list = service.findAll();
+        return list;
+    }
+
 }
