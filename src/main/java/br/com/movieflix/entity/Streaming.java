0391b9016230867ca03cd64ac1e845ec7522df7c
Diego Reis <diegoreso@me.com>
2025-05-27 19:59:24 -0300

    feat: adds streaming dto (request and response) and streaming mapper

--- src/main/java/br/com/movieflix/entity/Streaming.java
+++ src/main/java/br/com/movieflix/entity/Streaming.java
@@ -3,10 +3,11 @@ package br.com.movieflix.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
