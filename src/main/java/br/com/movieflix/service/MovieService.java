d9be9d018d4e666dde9cba91c60769dc78c209b4
Diego Reis <diegoreso@me.com>
2025-05-28 19:05:12 -0300

    feat: creates service to findMovieByCategory

--- src/main/java/br/com/movieflix/service/MovieService.java
+++ src/main/java/br/com/movieflix/service/MovieService.java
@@ -23,6 +23,10 @@ public class MovieService {
         return repository.findAll();
     }
 
+    public List<Movie> findByCategory(Long categoryId){
+        return repository.findMovieByCategories(List.of(Category.builder().id(categoryId).build()));
+    }
+
     public Movie insert(Movie movie){
         movie.setCategories(findCategories(movie.getCategories()));
         movie.setStreamings(findStreaming(movie.getStreamings()));
