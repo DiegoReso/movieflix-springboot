a8f9b149b759edafac9883189357c717ec995913
Diego Reis <diegoreso@me.com>
2025-05-28 11:26:35 -0300

    feat: creates MovieMapper

--- src/main/java/br/com/movieflix/mapper/MovieMapper.java
+++ src/main/java/br/com/movieflix/mapper/MovieMapper.java
@@ -1,4 +1,69 @@
 package br.com.movieflix.mapper;
 
+import br.com.movieflix.controller.request.MovieRequest;
+import br.com.movieflix.controller.response.CategoryResponse;
+import br.com.movieflix.controller.response.MovieResponse;
+import br.com.movieflix.controller.response.StreamingResponse;
+import br.com.movieflix.entity.Category;
+import br.com.movieflix.entity.Movie;
+import br.com.movieflix.entity.Streaming;
+import lombok.experimental.UtilityClass;
+
+
+import java.util.List;
+
+@UtilityClass
 public class MovieMapper {
+
+    public static Movie toMovie(MovieRequest movieRequest){
+
+        List<Category> categories = movieRequest.categories().stream()
+                .map(categoryID -> Category
+                        .builder()
+                        .id(categoryID)
+                        .build() )
+                .toList();
+
+        List<Streaming> streamings = movieRequest.streamings()
+                .stream().map(streamingID -> Streaming
+                        .builder()
+                        .id(streamingID)
+                        .build())
+                .toList();
+
+
+        return Movie
+                .builder()
+                .title(movieRequest.title())
+                .description(movieRequest.description())
+                .releaseDate(movieRequest.releaseDate())
+                .categories(categories)
+                .streamings(streamings)
+                .build();
+    }
+
+    public static MovieResponse toMovieResponse(Movie movie){
+
+        List<CategoryResponse> categories = movie.getCategories()
+                .stream()
+                .map(CategoryMapper::toCategoryRespose)
+                .toList();
+
+        List<StreamingResponse> streamings = movie.getStreamings()
+                .stream()
+                .map(StreamingMapper::toStreamingResponse)
+                .toList();
+
+
+        return MovieResponse
+                .builder()
+                .id(movie.getId())
+                .title(movie.getTitle())
+                .description(movie.getDescription())
+                .releaseDate(movie.getReleaseDate())
+                .rating(movie.getRating())
+                .categories(categories)
+                .streaming(streamings)
+                .build();
+    }
 }
