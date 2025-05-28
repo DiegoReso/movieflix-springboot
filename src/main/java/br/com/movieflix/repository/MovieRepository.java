c1bf9ff54dc9d63c0074dc18d10fdfef1b37c13b
Diego Reis <diegoreso@me.com>
2025-05-28 19:04:21 -0300

    feat: creates List findMovieByCategories

--- src/main/java/br/com/movieflix/repository/MovieRepository.java
+++ src/main/java/br/com/movieflix/repository/MovieRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie,Long> {
+
+    List<Movie> findMovieByCategories(List<Category> categories);
+
 }
