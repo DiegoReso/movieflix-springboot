d48fb095c09dd778ef5d09dfeff2ca99ad407ac2
Diego Reis <diegoreso@me.com>
2025-05-30 17:28:01 -0300

    feat: adds swagger configuration

---
+++ src/main/java/br/com/movieflix/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package br.com.movieflix.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "MovieFlix API",
+                version = "1.0.0",
+                description = "Documentação da API do MovieFlix, uma plataforma para gerenciar filmes e usuários.",
+                contact = @Contact(
+                        name = "Diego Reis",
+                        email = "diegoreso@me.com",
+                        url = "movieflix.diego.reis.dev (descritiva apenas para fins de estudo)"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.html"
+                )
+        ),
+        servers = {
+                @Server(url = "http://localhost:8080", description = "Servidor de Desenvolvimento Local")
+        }
+)
+public class SwaggerConfig {
+
+}
