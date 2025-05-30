cba33b99d30cf8a7264e10b077fa0da4b97427a2
Diego Reis <diegoreso@me.com>
2025-05-30 16:14:45 -0300

    refactor: adds @Valid to validate categoryRequest directly in the request

--- src/main/java/br/com/movieflix/controller/CategoryController.java
+++ src/main/java/br/com/movieflix/controller/CategoryController.java
@@ -6,6 +6,7 @@ import br.com.movieflix.controller.response.CategoryResponse;
 import br.com.movieflix.entity.Category;
 import br.com.movieflix.mapper.CategoryMapper;
 import br.com.movieflix.service.CategoryService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,7 +32,7 @@ public class CategoryController {
     }
 
     @PostMapping
-    public ResponseEntity<CategoryResponse>  insert(@RequestBody CategoryRequest categoryRequest){
+    public ResponseEntity<CategoryResponse>  insert(@Valid @RequestBody CategoryRequest categoryRequest){
         CategoryResponse categoryResponse =
                 CategoryMapper.toCategoryRespose(service.insert(CategoryMapper.toCategory(categoryRequest)));
 
