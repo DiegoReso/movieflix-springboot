34dbc80f87ea56d49578a09295076ee2902477ae
Diego Reis <diegoreso@me.com>
2025-05-30 16:20:39 -0300

    refactor: adds @NotEmpty to validate streamingRequest directly in the request

--- src/main/java/br/com/movieflix/controller/request/StreamingRequest.java
+++ src/main/java/br/com/movieflix/controller/request/StreamingRequest.java
@@ -1,5 +1,7 @@
 package br.com.movieflix.controller.request;
 
 
-public record StreamingRequest(String name) {
+import jakarta.validation.constraints.NotEmpty;
+
+public record StreamingRequest(@NotEmpty(message = "Nome da streaming obrigatório") String name) {
 }
