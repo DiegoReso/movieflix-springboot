4499eda88aadc859bfdb7516ee138e1bb91a5ba4
Diego Reis <diegoreso@me.com>
2025-05-30 16:16:43 -0300

    refactor: adds @NotEmpty to validate loginRequest directly in the request

--- src/main/java/br/com/movieflix/controller/request/LoginRequest.java
+++ src/main/java/br/com/movieflix/controller/request/LoginRequest.java
@@ -1,4 +1,7 @@
 package br.com.movieflix.controller.request;
 
-public record LoginRequest(String email, String password) {
+import jakarta.validation.constraints.NotEmpty;
+
+public record LoginRequest(@NotEmpty(message = "Email obrigatório") String email,
+                           @NotEmpty(message = "Password obrigatório") String password) {
 }
