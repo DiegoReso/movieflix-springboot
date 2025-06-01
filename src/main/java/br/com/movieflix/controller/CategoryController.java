f85b004cc621e7345e35491dbe6064547e609b57
Diego Reis <diegoreso@me.com>
2025-05-31 22:12:04 -0300

    feat: adds operation and api repsonse for api documentation

--- src/main/java/br/com/movieflix/controller/CategoryController.java
+++ src/main/java/br/com/movieflix/controller/CategoryController.java
@@ -3,9 +3,16 @@ package br.com.movieflix.controller;
 
 import br.com.movieflix.controller.request.CategoryRequest;
 import br.com.movieflix.controller.response.CategoryResponse;
-import br.com.movieflix.entity.Category;
+
+import br.com.movieflix.controller.response.MovieResponse;
 import br.com.movieflix.mapper.CategoryMapper;
 import br.com.movieflix.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,11 +26,16 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/movieflix/category")
 @RequiredArgsConstructor
+@Tag (name = "Categorias", description = "Endpoints para gerenciamento de categorias")
 public class CategoryController {
 
 
     private final CategoryService service;
 
+    @Operation (summary = "Buscar todas as categorias", description = "Endpoint para buscar todas as categorias cadastradas")
+    @ApiResponse(responseCode = "200", description = "Lista de categorias retornada com sucesso",
+            content = @Content(array = @ArraySchema(schema = @Schema(implementation = CategoryResponse.class)))
+    )
     @GetMapping
     public ResponseEntity<List<CategoryResponse>>  getAllCategories(){
         List<CategoryResponse> categoryResponseList = service.findAll()
@@ -32,6 +44,10 @@ public class CategoryController {
         return ResponseEntity.ok(categoryResponseList);
     }
 
+    @Operation (summary = "Inserir uma nova categoria", description = "Endpoint para inserir uma nova categoria no sistema")
+    @ApiResponse(responseCode = "201", description = "Categoria inserido com sucesso",
+            content = @Content(schema = @Schema(implementation = CategoryResponse.class))
+    )
     @PostMapping
     public ResponseEntity<CategoryResponse>  insert(@Valid @RequestBody CategoryRequest categoryRequest){
         CategoryResponse categoryResponse =
@@ -40,9 +56,15 @@ public class CategoryController {
         return ResponseEntity.status(HttpStatus.CREATED).body(categoryResponse);
     }
 
+    @Operation (summary = "Buscar categoria por ID", description = "Endpoint para buscar uma categoria específica pelo ID")
+    @ApiResponse(responseCode = "200", description = "Categoria encontrada com sucesso",
+            content = @Content(schema = @Schema(implementation = CategoryResponse.class))
+    )
+    @ApiResponse (responseCode = "404", description = "Categoria não encontrada",
+            content = @Content(schema = @Schema(implementation = String.class))
+    )
     @GetMapping("/{id}")
     public ResponseEntity<CategoryResponse>  getCategoryById(@PathVariable Long id){
-
 
             return service.getCategoryById(id)
                     .map(category -> ResponseEntity.ok(CategoryMapper.toCategoryRespose(category)))
@@ -50,6 +72,13 @@ public class CategoryController {
 
     }
 
+    @Operation (summary = "Deletar categoria por ID", description = "Endpoint para deletar uma categoria específica pelo ID")
+    @ApiResponse(responseCode = "204", description = "Categoria deletada com sucesso",
+            content = @Content(schema = @Schema(type = "void"))
+    )
+    @ApiResponse(responseCode = "404", description = "Categoria não encontrada",
+            content = @Content(schema = @Schema(type = "string"))
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCategory(@PathVariable Long id){
         service.delete(id);
--- src/main/java/br/com/movieflix/controller/StreamingController.java
+++ src/main/java/br/com/movieflix/controller/StreamingController.java
@@ -4,7 +4,14 @@ import br.com.movieflix.controller.request.StreamingRequest;
 import br.com.movieflix.controller.response.StreamingResponse;
 import br.com.movieflix.mapper.StreamingMapper;
 import br.com.movieflix.service.StreamingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.Table;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,10 +24,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/movieflix/streaming")
 @RequiredArgsConstructor
+@Tag(name = "Streamings", description = "Endpoints para gerenciamento de streamings")
 public class StreamingController {
 
     private final StreamingService service;
 
+    @Operation (summary = "Buscar todas as streamings", description = "Endpoint para buscar todas as streamings cadastradas")
+    @ApiResponse( responseCode = "200", description = "Lista de streamings retornada com sucesso",
+    content =  @Content(array = @ArraySchema(schema = @Schema(implementation = StreamingResponse.class))))
     @GetMapping
     public ResponseEntity<List<StreamingResponse>>  findAllStreamings(){
             List<StreamingResponse> streamingResponse =  service.findAll().stream()
@@ -30,6 +41,9 @@ public class StreamingController {
             return ResponseEntity.ok(streamingResponse);
     }
 
+    @Operation (summary = "Inserir uma nova streaming", description = "Endpoint para inserir uma nova streaming no sistema")
+    @ApiResponse(responseCode = "201", description = "Streaming inserida com sucesso",
+            content = @Content(schema = @Schema(implementation = StreamingResponse.class)))
     @PostMapping
     public ResponseEntity<StreamingResponse> insertStreaming(@Valid  @RequestBody StreamingRequest streamingRequest){
         StreamingResponse streaming = StreamingMapper.toStreamingResponse(service.insertStreaming(StreamingMapper.toStreaming(streamingRequest)));
@@ -37,6 +51,11 @@ public class StreamingController {
         return ResponseEntity.status(HttpStatus.CREATED).body(streaming);
     }
 
+    @Operation (summary = "Buscar streaming por ID", description = "Endpoint para buscar uma streaming específica pelo ID")
+    @ApiResponse(responseCode = "200", description = "Streaming encontrada com sucesso",
+            content = @Content(schema = @Schema(implementation = StreamingResponse.class)))
+    @ApiResponse (responseCode = "404", description = "Streaming não encontrada",
+            content = @Content(schema = @Schema(implementation = String.class)))
     @GetMapping("/{id}")
     public ResponseEntity<StreamingResponse> findStreamingById(@PathVariable Long id){
         return service.findById(id)
@@ -44,6 +63,9 @@ public class StreamingController {
                 .orElseThrow(() -> new EntityNotFoundException("Streaming não encontrado com o id: " + id));
     }
 
+    @Operation (summary = "Atualizar uma streaming", description = "Endpoint para atualizar uma streaming existente")
+    @ApiResponse (responseCode = "204", description = "Streaming deletada com sucesso",
+            content = @Content(schema = @Schema(implementation = StreamingResponse.class)))
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteStreaming(@PathVariable Long id){
         service.delete(id);
