54682bdfe1a8e4ea30fad417473ff7a1c15a5b53
Diego Reis <diegoreso@me.com>
2025-05-31 22:19:49 -0300

    feat: adds operation and api response for api documentation

--- src/main/java/br/com/movieflix/controller/AuthController.java
+++ src/main/java/br/com/movieflix/controller/AuthController.java
@@ -9,6 +9,11 @@ import br.com.movieflix.entity.User;
 import br.com.movieflix.exception.UsernameOrPasswordInvalidException;
 import br.com.movieflix.mapper.UserMapper;
 import br.com.movieflix.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -25,18 +30,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("movieflix/auth")
 @RequiredArgsConstructor
+@Tag( name = "Autenticação", description = "Endpoints para autenticação de usuários")
 public class AuthController {
 
     private final UserService service;
     private final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
 
+    @Operation (summary = "Registrar um novo usuário", description = "Endpoint para registrar um novo usuário no sistema")
+    @ApiResponse (responseCode = "201", description = "Usuário registrado com sucesso",
+            content = @Content(schema = @Schema(implementation = UserResponse.class)))
+    @ApiResponse (responseCode = "400", description = "Dados inválidos",
+            content = @Content(schema = @Schema(implementation = String.class)))
     @PostMapping("/register")
     public ResponseEntity<UserResponse>  register(@Valid @RequestBody UserRequest request){
         UserResponse userResponse = UserMapper.toUserResponse(service.save(UserMapper.toUser(request)));
         return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
     }
 
+    @Operation (summary = "Login de usuário", description = "Endpoint para realizar o login de um usuário no sistema")
+    @ApiResponse (responseCode = "200", description = "Login realizado com sucesso",
+            content = @Content(schema = @Schema(implementation = LoginResponse.class)))
+    @ApiResponse (responseCode = "401", description = "Usuário ou senha inválidos",
+            content = @Content(schema = @Schema(implementation = String.class)))
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request){
 
