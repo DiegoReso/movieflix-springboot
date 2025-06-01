850b6567f07a3b770edeac6260e864611fc192ee
Diego Reis <diegoreso@me.com>
2025-05-31 22:20:33 -0300

    feat: adds @NotEmpty to validates name and email as well

--- src/main/java/br/com/movieflix/controller/request/UserRequest.java
+++ src/main/java/br/com/movieflix/controller/request/UserRequest.java
@@ -1,14 +1,17 @@
 package br.com.movieflix.controller.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 
 public record UserRequest(
         @Schema (description = "ID do usuário", example = "1", type = "Long")
+        @NotEmpty (message = "ID do usuário obrigatório")
         String name,
         @Schema (description = "Email do usuário", example = "email@email.com")
+        @NotEmpty(message = "Email obrigatório")
         String email,
         @Schema (description = "Senha do usuário", example = "senha123")
-                        @jakarta.validation.constraints.NotEmpty(message = "Senha obrigatória")
+        @NotEmpty(message = "Senha obrigatória")
         String password
 ){
 }
