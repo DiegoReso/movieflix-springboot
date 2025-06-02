c30cf95a105c8f7b9e09794f75849290b97c7b5c
Diego Reis <diegoreso@me.com>
2025-06-02 10:15:10 -0300

    feat: creates MovieController interface to organize the code

--- src/main/java/br/com/movieflix/controller/MovieController.java
+++ src/main/java/br/com/movieflix/controller/MovieController.java
@@ -1,36 +1,21 @@
 package br.com.movieflix.controller;
 
-
 import br.com.movieflix.controller.request.MovieRequest;
 import br.com.movieflix.controller.response.MovieResponse;
-import br.com.movieflix.entity.Movie;
-import br.com.movieflix.mapper.MovieMapper;
-import br.com.movieflix.service.MovieService;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.nio.channels.NotYetBoundException;
 import java.util.List;
-import java.util.Optional;
 
-@RestController
-@RequestMapping("/movieflix/movie")
-@RequiredArgsConstructor
-@Tag(name = "Filmes", description = "Endpoints para gerenciamento de filmes")
-public class MovieController {
-
-    private final MovieService service;
+public interface MovieController {
 
     @Operation(summary = "Inserir um novo filme", description = "Endpoint para inserir um novo filme no sistema",
             security = @SecurityRequirement(name = "bearerAuth")
@@ -39,11 +24,8 @@ public class MovieController {
             content = @Content(schema = @Schema(implementation = MovieResponse.class))
     )
     @PostMapping
-    public ResponseEntity<MovieResponse> insert(@Valid @RequestBody MovieRequest movieRequest){
-        Movie movie = service.insert(MovieMapper.toMovie(movieRequest));
-        MovieResponse movieResponse = MovieMapper.toMovieResponse(movie);
-        return ResponseEntity.status(HttpStatus.CREATED).body(movieResponse);
-    }
+     ResponseEntity<MovieResponse> insert(@Valid @RequestBody MovieRequest movieRequest);
+
 
     @Operation(summary = "Buscar todos os filmes", description = "Endpoint para buscar todos os filmes cadastrados",
             security = @SecurityRequirement(name = "bearerAuth")
@@ -52,13 +34,7 @@ public class MovieController {
             content = @Content(array = @ArraySchema(schema = @Schema(implementation = MovieResponse.class)))
     )
     @GetMapping
-    public ResponseEntity<List<MovieResponse>> findAll(){
-        List<Movie> movies = service.findAll();
-        List<MovieResponse> movieResponses = movies.stream()
-                .map(MovieMapper::toMovieResponse)
-                .toList();
-        return ResponseEntity.ok().body(movieResponses);
-    }
+    ResponseEntity<List<MovieResponse>> findAll();
 
     @Operation(summary = "Buscar filme por ID", description = "Endpoint para buscar um filme específico pelo ID",
             security = @SecurityRequirement(name = "bearerAuth")
@@ -70,11 +46,7 @@ public class MovieController {
             content = @Content(schema = @Schema(type = "string"))
     )
     @GetMapping("/{id}")
-    public ResponseEntity<MovieResponse> findById(@PathVariable Long id){
-        return service.findById(id)
-                .map(movie -> ResponseEntity.ok(MovieMapper.toMovieResponse(movie)))
-                .orElseThrow(() -> new EntityNotFoundException("Filme não encontrado com o id: " + id));
-    }
+    ResponseEntity<MovieResponse> findById(@PathVariable Long id);
 
 
     @Operation(summary = "Deletar filme por ID", description = "Endpoint para deletar um filme específico pelo ID",
@@ -85,15 +57,8 @@ public class MovieController {
             content = @Content(schema = @Schema(type = "string"))
     )
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteById(@PathVariable Long id){
-        return service.findById(id)
-                .map(movie -> {
-                    service.delete(movie.getId());
-                    return ResponseEntity.noContent().build();
-                })
-                .orElseThrow(() -> new EntityNotFoundException("Filme não encontrado com o id: " + id));
+    ResponseEntity<?> deleteById(@PathVariable Long id);
 
-    }
 
     @Operation (summary = "Atualizar filme por ID", description = "Endpoint para atualizar um filme específico pelo " +
             "ID",
@@ -106,11 +71,7 @@ public class MovieController {
             content = @Content(schema = @Schema(type = "string"))
     )
     @PutMapping("/{id}")
-    public ResponseEntity<MovieResponse> update(@Valid @RequestBody MovieRequest movieRequest, @PathVariable Long id){
-        return service.update(MovieMapper.toMovie(movieRequest), id)
-                .map(movie -> ResponseEntity.ok(MovieMapper.toMovieResponse(movie)))
-                .orElseThrow(() -> new EntityNotFoundException("Filme não encontrado com o id: " + id));
-    }
+    ResponseEntity<MovieResponse> update(@Valid @RequestBody MovieRequest movieRequest, @PathVariable Long id);
 
     @Operation (summary = "Buscar filmes por categoria", description = "Endpoint para buscar filmes filtrados por " +
             "categoria",
@@ -123,10 +84,6 @@ public class MovieController {
             content = @Content(schema = @Schema(type = "string"))
     )
     @GetMapping("/search")
-    public ResponseEntity<List<MovieResponse>> getMovieParam(@RequestParam Long category){
-        return ResponseEntity.ok(service.findByCategory(category).stream()
-                .map(MovieMapper::toMovieResponse)
-                .toList());
-    }
+    ResponseEntity<List<MovieResponse>> getMovieParam(@RequestParam Long category);
 
 }
